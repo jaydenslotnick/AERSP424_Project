aefbe947b323e441375fbfe63c28cb98a89709fe
jaydenslotnick <jcs6472@psu.edu>
2024-02-12 12:07:53 -0500

    Added league class that takes user input for members, creates a league and outputs league members

---
+++ League.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+
+class League {
+private:
+    int numMembers;
+    std::string leagueName;
+    std::vector<std::string> leagueMembers;
+    
+
+public:
+    // Constructor
+    League(const std::string& name, int numTeams) : leagueName(name), numMembers(numTeams), leagueMembers(numTeams) {
+        std::cout << "League created! " << std::endl;
+    };
+
+    void addMembers() {
+
+
+        for (int i = 0; i < numMembers; ++i) {
+            std::string name;
+            std::cout << "Enter name for member " << (i + 1) << ": ";
+            std::cin >> name;
+            leagueMembers[i] = name;
+        }
+        std::cout << "Welcome to " << leagueName << " Fantasy Football League\n";
+        std::cout << "Fantasy Football League Members:\n";
+
+        for (int i = 0; i < numMembers; ++i) {
+            std::cout << "Member " << (i + 1) << ": " << leagueMembers[i] << std::endl;
+        }
+    }
+
+
+
+
+};
--- main.cpp
+++ main.cpp
@@ -2,12 +2,26 @@
 #include <fstream>
 #include <string>
 #include "Ranking.h"
+#include "League.h"
 
 int main()
 {
-	ranking myRanking;
+	// calling league class and calling addMember function, prints out league
+	std::string leagueName;
+	int numMembers;
+	std::cout << "Enter league name: " << std::endl;
+	std::cin >> leagueName;
+	std::cout << "Enter number of members: " << std::endl;
+	std::cin >> numMembers;
+	League myLeague(leagueName, numMembers);
+	myLeague.addMembers();
 
+	// calling ranking class and calling operate function
+	ranking myRanking;
 	myRanking.operate();
+
+
+
 
 	return 0;
 }
