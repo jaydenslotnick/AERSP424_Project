33a75fd8c50efddcc8d4839a75e385f7895ab236
payton-glynn <pjglynn29@gmail.com>
2024-02-26 20:42:46 -0500

    captured edge cases for league name and league members

--- League.h
+++ League.h
@@ -21,11 +21,46 @@ public:
 
     // Asks for user input of the league
     void getLeagueInfo() {
-        std::cout << "Enter league name: " << std::endl;
-        std::getline(std::cin, leagueName); // Allowing the league name to be multiple words
+        bool validLeagueName = false;
+        while (!validLeagueName)
+        {
+            std::cout << "Enter league name: " << std::endl;
+            std::getline(std::cin, leagueName); // Allowing the league name to be multiple words
 
-        std::cout << "Enter number of members: " << std::endl;
-        std::cin >> numMembers;
+            validLeagueName = true;
+            for (char c : leagueName)       // iterates over each character in the string leagueName
+            {
+                if (!std::isalpha(c) && c != ' ')       // make sure the user enters only letters or spaces for the league name
+                {
+                    validLeagueName = false;
+                    break;
+                }
+            }
+            if (!validLeagueName)                     // prints an error message if the user enters an invalid name
+            {
+                std::cout << "Please enter a valid league name consisting only of letters and spaces." << std::endl;
+            }
+        }
+        std::cout << "League name entered: " << leagueName << std::endl;
+
+        bool validNumMembers = false;        
+        while (!validNumMembers)
+        {
+            std::cout << "Enter number of members (up to 10): " << std::endl;
+            std::cin >> numMembers;
+
+            if (std::cin.fail() || numMembers <= 0 || numMembers > 10)
+            {
+                std::cout << "Please enter a positive integer up to 10." << std::endl;
+                std::cin.clear();       // clear the error flag
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');     // discard invalid input
+            }
+            else
+            {
+                validNumMembers = true;
+            }
+        }
+        std::cout << "You entered: " << numMembers << std::endl;
     }
 
 
